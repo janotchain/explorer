7350f1a8bfd61d1110593e45ebc36dc88f5c1868
janotchain <onosmediang@gmail.com>
2022-08-12 11:15:14 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,24 +5,24 @@
 
 # see LICENSE for details
 
-HEIGHT=$(curl -s http://localhost:11898/getheight | jq '.height')
+HEIGHT=$(curl -s http://localhost:10124/getheight | jq '.height')
 let "HEIGHT--"
 
 cat ascii.txt
 
 echo -e "\n### CURRENT BLOCK ###"
-curl -s http://localhost:11898/getheight | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
+curl -s http://localhost:10124/getheight | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
 
 echo -e "\n### LAST BLOCK HEADER ###"
-curl -sd '{"jsonrpc":"2.0","method":"getlastblockheader","params":{}}' http://localhost:11898/json_rpc | jq -r .result.block_header | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
+curl -sd '{"jsonrpc":"2.0","method":"getlastblockheader","params":{}}' http://localhost:10124/json_rpc | jq -r .result.block_header | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]'
 
 echo -e "\n### NETWORK INFO ###"
-curl -s http://localhost:11898/getinfo | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
+curl -s http://localhost:10124/getinfo | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
 
 echo -e "\n### PENDING TRANSACTIONS ###"
-curl -sd '{"jsonrpc":"2.0","method":"f_on_transactions_pool_json","params":{}}' http://localhost:11898/json_rpc | jq '.result.transactions[]' | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
+curl -sd '{"jsonrpc":"2.0","method":"f_on_transactions_pool_json","params":{}}' http://localhost:10124/json_rpc | jq '.result.transactions[]' | jq -r 'to_entries|map("\(.key) - \(.value|tostring)")|.[]' 
 
 echo -e "\n### LAST 10 BLOCKS ###"
-curl -sd '{"jsonrpc":"2.0","method":"f_blocks_list_json","params":{"height":'"$HEIGHT"'}}' http://localhost:11898/json_rpc | jq '.result.blocks' | jq '.[0:10]'
+curl -sd '{"jsonrpc":"2.0","method":"f_blocks_list_json","params":{"height":'"$HEIGHT"'}}' http://localhost:10124/json_rpc | jq '.result.blocks' | jq '.[0:10]'
 
 cat footer.txt
